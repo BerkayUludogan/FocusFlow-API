140f05b4279528c68fbdd4a08b5baf4c992333bb
Berkay Uludoğan <buludogan0@gmail.com>
2026-06-20 19:29:50 +0300

    Configure EF Core DbContext with PostgreSQL

---
+++ src/FocusFlow.Api/Persistence/FocusFlowDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FocusFlow.Api.Persistence;
+
+public sealed class FocusFlowDbContext(DbContextOptions<FocusFlowDbContext> options) : DbContext(options)
+{
+
+}
+
--- src/FocusFlow.Api/Program.cs
+++ src/FocusFlow.Api/Program.cs
@@ -1,6 +1,13 @@
+using FocusFlow.Api.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<FocusFlowDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "FocusFlow API");
 
 app.Run();
