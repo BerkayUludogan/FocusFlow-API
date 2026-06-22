34d745115e8f33f092840b543ce4a041248a10f6
Berkay Uludoğan <buludogan0@gmail.com>
2026-06-22 14:45:27 +0300

    Add pomodoro session entity

---
+++ src/FocusFlow.Api/Domain/Entities/PomodoroSessionEntity.cs
@@ -0,0 +1,27 @@
+﻿using FocusFlow.Api.Domain.Entities.Base;
+using FocusFlow.Api.Domain.Enums;
+
+namespace FocusFlow.Api.Domain.Entities;
+
+public sealed class PomodoroSessionEntity : BaseEntity
+{
+    public Guid UserId { get; set; }
+
+    public Guid? TaskItemId { get; set; }
+
+    public Guid ClientId { get; set; }
+
+    public PomodoroSessionType Type { get; set; }
+
+    public PomodoroSessionStatus Status { get; set; }
+
+    public DateTime StartedAtUtc { get; set; }
+
+    public DateTime? EndedAtUtc { get; set; }
+
+    public int DurationMinutes { get; set; }
+
+    public UserEntity User { get; set; } = null!;
+
+    public TaskItemEntity? TaskItem { get; set; }
+}
---
+++ src/FocusFlow.Api/Domain/Enums/PomodoroSessionStatus.cs
@@ -0,0 +1,8 @@
+﻿namespace FocusFlow.Api.Domain.Enums;
+
+public enum PomodoroSessionStatus
+{
+    Running = 1,
+    Completed = 2,
+    Cancelled = 3,
+}
