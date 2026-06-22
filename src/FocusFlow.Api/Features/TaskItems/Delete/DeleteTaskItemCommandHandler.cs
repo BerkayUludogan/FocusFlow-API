d6cc30f9b64f2522b5c2b22ff85fca851da9b2f8
Berkay Uludoğan <buludogan0@gmail.com>
2026-06-22 14:23:57 +0300

    Add delete task item slice

---
+++ src/FocusFlow.Api/Features/TaskItems/Delete/DeleteTaskItemCommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using FocusFlow.Api.Features.TaskItems.Rules;
+using FocusFlow.Api.Persistence.Context;
+using MediatR;
+
+namespace FocusFlow.Api.Features.TaskItems.Delete
+{
+    public class DeleteTaskItemCommandHandler
+        (FocusFlowDbContext dbContext, ITaskItemBusinessRules rules) :
+        IRequestHandler<DeleteTaskItemCommandRequest, DeleteTaskItemCommandResponse>
+    {
+        public async Task<DeleteTaskItemCommandResponse> Handle(DeleteTaskItemCommandRequest request, CancellationToken cancellationToken)
+        {
+            var taskItem = await rules.TaskItemMustExistAsync(request.UserId, request.Id, cancellationToken);
+
+            taskItem.IsDeleted = true;
+            taskItem.ModifiedAtUtc = DateTime.UtcNow;
+
+            await dbContext.SaveChangesAsync(cancellationToken);
+
+            return new DeleteTaskItemCommandResponse
+            {
+                Id = request.Id,
+            };
+        }
+    }
+}
