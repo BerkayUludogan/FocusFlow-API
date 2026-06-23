85ce3aa7533666bbf4d612b3a2c301d387543ccf
Berkay Uludoğan <buludogan0@gmail.com>
2026-06-23 12:24:35 +0300

    Add get running pomodoro session slice

---
+++ src/FocusFlow.Api/Features/PomodoroSessions/GetRunning/GetRunningPomodoroSessionEndpoint.cs
@@ -0,0 +1,25 @@
+﻿using FocusFlow.Api.Extensions;
+using FocusFlow.Api.Shared.EndPoints;
+using MediatR;
+
+namespace FocusFlow.Api.Features.PomodoroSessions.GetRunning;
+
+public sealed class GetRunningPomodoroSessionEndpoint : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/api/pomodoro-sessions/running", async (
+            HttpContext httpContext,
+            ISender sender,
+            CancellationToken cancellationToken
+            ) =>
+        {
+            var response = await sender.Send(new GetRunningPomodoroSessionQueryRequest
+            {
+                UserId = httpContext.User.GetUserId()
+            },cancellationToken);
+            return Results.Ok(response);
+
+        }).WithTags("Pomodoro Sessions").RequireAuthorization();
+    }
+}
