be1e14dbc18705e4e031953e73d7a0a16bee9940
Berkay Uludoğan <buludogan0@gmail.com>
2026-06-23 15:59:41 +0300

    Add update user profile slice

---
+++ src/FocusFlow.Api/Features/Users/Profile/Update/UpdateUserProfileCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using FocusFlow.Api.Domain.Constants.FieldLengths;
+
+namespace FocusFlow.Api.Features.Users.Profile.Update;
+
+public sealed class UpdateUserProfileCommandValidator : AbstractValidator<UpdateUserProfileCommandRequest>
+{
+    public UpdateUserProfileCommandValidator()
+    {
+        RuleFor(request => request.DisplayName)
+            .NotEmpty()
+            .MinimumLength(2)
+            .MaximumLength(UserFieldLengths.DisplayName);
+    }
+}
---
+++ src/FocusFlow.Api/Features/Users/Profile/Update/UpdateUserProfileEndpoint.cs
@@ -0,0 +1,25 @@
+﻿using FocusFlow.Api.Extensions;
+using FocusFlow.Api.Shared.EndPoints;
+using MediatR;
+
+namespace FocusFlow.Api.Features.Users.Profile.Update;
+
+public sealed class UpdateUserProfileEndpoint : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapPut("/api/users/profile", async (
+            UpdateUserProfileCommandRequest request,
+            HttpContext httpContext,
+            ISender sender,
+            CancellationToken cancellationToken
+            ) =>
+        {
+            request.UserId = httpContext.User.GetUserId();
+            var response = await sender.Send(request, cancellationToken);
+
+            return Results.Ok(response);
+
+        }).WithTags("Users").RequireAuthorization();
+    }
+}
