32a743665cf82b60033c27cb8600b0c23843ffc8
Berkay Uludoğan <buludogan0@gmail.com>
2026-06-23 18:19:24 +0300

    Add email verification token service

---
+++ src/FocusFlow.Api/Shared/Abstractions/Email/IEmailVerificationTokenService.cs
@@ -0,0 +1,8 @@
+﻿namespace FocusFlow.Api.Shared.Abstractions.Email;
+
+public interface IEmailVerificationTokenService
+{
+    EmailVerificationTokenDto CreateToken();
+
+    string HashToken(string token);
+}
